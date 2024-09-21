7672b11289c7f63c3519e42ee0d3ef3064fa8bb8
Juhyeong Ko <dury.ko@gmail.com>
2024-09-21 22:24:41 +0900

    fix: disable spring security auto config for now

--- src/main/java/com/nano_sns/nano_sns/NanoSnsJavaApplication.java
+++ src/main/java/com/nano_sns/nano_sns/NanoSnsJavaApplication.java
@@ -2,8 +2,9 @@ package com.nano_sns.nano_sns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "com.nano_sns")
+@SpringBootApplication(scanBasePackages = "com.nano_sns", exclude = { SecurityAutoConfiguration.class })
 public class NanoSnsJavaApplication {
 
 	public static void main(String[] args) {
