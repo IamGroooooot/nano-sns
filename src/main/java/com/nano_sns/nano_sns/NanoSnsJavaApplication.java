635961b6dd09b158adae48a6e1644df96b128e32
Juhyeong Ko <dury.ko@gmail.com>
2024-09-21 21:36:27 +0900

    feat: add basic directories

--- src/main/java/com/nano_sns/nano_sns/NanoSnsJavaApplication.java
+++ src/main/java/com/nano_sns/nano_sns/NanoSnsJavaApplication.java
@@ -3,7 +3,7 @@ package com.nano_sns.nano_sns;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.nano_sns")
 public class NanoSnsJavaApplication {
 
 	public static void main(String[] args) {
